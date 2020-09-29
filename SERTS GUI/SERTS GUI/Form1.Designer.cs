d34603c7c2ec0b447d3cc7b81544a867690d4164
Zachariah Jones <zach.jones@eagles.oc.edu>
2020-09-28 23:54:41 -0500

    First initial commit. This allows the user to see a GUI that has a play, pause, skip, back and a few more buttons. They are purely aesthetic.

---
+++ SERTS GUI/SERTS GUI/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SERTS_GUI
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.PlayButton = new System.Windows.Forms.Button();
+			this.ffButton = new System.Windows.Forms.Button();
+			this.rwButton = new System.Windows.Forms.Button();
+			this.skipForwardButton = new System.Windows.Forms.Button();
+			this.skipBackButton = new System.Windows.Forms.Button();
+			this.PauseButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// PlayButton
+			// 
+			this.PlayButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.PlayButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.PlayButton.Image = ((System.Drawing.Image)(resources.GetObject("PlayButton.Image")));
+			this.PlayButton.Location = new System.Drawing.Point(384, 381);
+			this.PlayButton.Name = "PlayButton";
+			this.PlayButton.Size = new System.Drawing.Size(54, 57);
+			this.PlayButton.TabIndex = 0;
+			this.PlayButton.UseVisualStyleBackColor = false;
+			this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+			// 
+			// ffButton
+			// 
+			this.ffButton.Image = ((System.Drawing.Image)(resources.GetObject("ffButton.Image")));
+			this.ffButton.Location = new System.Drawing.Point(444, 381);
+			this.ffButton.Name = "ffButton";
+			this.ffButton.Size = new System.Drawing.Size(68, 57);
+			this.ffButton.TabIndex = 1;
+			this.ffButton.UseVisualStyleBackColor = true;
+			this.ffButton.Click += new System.EventHandler(this.ffButton_Click);
+			// 
+			// rwButton
+			// 
+			this.rwButton.Image = ((System.Drawing.Image)(resources.GetObject("rwButton.Image")));
+			this.rwButton.Location = new System.Drawing.Point(305, 381);
+			this.rwButton.Name = "rwButton";
+			this.rwButton.Size = new System.Drawing.Size(73, 57);
+			this.rwButton.TabIndex = 2;
+			this.rwButton.UseVisualStyleBackColor = true;
+			// 
+			// skipForwardButton
+			// 
+			this.skipForwardButton.Image = ((System.Drawing.Image)(resources.GetObject("skipForwardButton.Image")));
+			this.skipForwardButton.Location = new System.Drawing.Point(518, 381);
+			this.skipForwardButton.Name = "skipForwardButton";
+			this.skipForwardButton.Size = new System.Drawing.Size(59, 57);
+			this.skipForwardButton.TabIndex = 3;
+			this.skipForwardButton.UseVisualStyleBackColor = true;
+			// 
+			// skipBackButton
+			// 
+			this.skipBackButton.Image = ((System.Drawing.Image)(resources.GetObject("skipBackButton.Image")));
+			this.skipBackButton.Location = new System.Drawing.Point(245, 381);
+			this.skipBackButton.Name = "skipBackButton";
+			this.skipBackButton.Size = new System.Drawing.Size(54, 57);
+			this.skipBackButton.TabIndex = 4;
+			this.skipBackButton.UseVisualStyleBackColor = true;
+			// 
+			// PauseButton
+			// 
+			this.PauseButton.Image = ((System.Drawing.Image)(resources.GetObject("PauseButton.Image")));
+			this.PauseButton.Location = new System.Drawing.Point(384, 382);
+			this.PauseButton.Name = "PauseButton";
+			this.PauseButton.Size = new System.Drawing.Size(54, 55);
+			this.PauseButton.TabIndex = 5;
+			this.PauseButton.UseVisualStyleBackColor = true;
+			this.PauseButton.Visible = false;
+			this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.PauseButton);
+			this.Controls.Add(this.skipBackButton);
+			this.Controls.Add(this.skipForwardButton);
+			this.Controls.Add(this.rwButton);
+			this.Controls.Add(this.ffButton);
+			this.Controls.Add(this.PlayButton);
+			this.ForeColor = System.Drawing.SystemColors.Control;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button PlayButton;
+		private System.Windows.Forms.Button ffButton;
+		private System.Windows.Forms.Button rwButton;
+		private System.Windows.Forms.Button skipForwardButton;
+		private System.Windows.Forms.Button skipBackButton;
+		private System.Windows.Forms.Button PauseButton;
+	}
+}
+
---
+++ SERTS GUI/SERTS GUI/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace SERTS_GUI
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void PlayButton_Click(object sender, EventArgs e)
+		{
+			PlayButton.Hide();
+			PauseButton.Show();
+		}
+
+		private void ffButton_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void PauseButton_Click(object sender, EventArgs e)
+		{
+			PauseButton.Hide();
+			PlayButton.Show();
+		}
+	}
+}
