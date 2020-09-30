c29a2c0d52016adc62c5a6aac6bb5d9629a0089f
Zachariah Jones <zach.jones@eagles.oc.edu>
2020-09-30 10:42:56 -0500

    Polished up UI. Added the serial port viewer. Haven't tested yet since I don't have my board.

--- SERTS GUI/SERTS GUI/Form1.Designer.cs
+++ SERTS GUI/SERTS GUI/Form1.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.PlayButton = new System.Windows.Forms.Button();
 			this.ffButton = new System.Windows.Forms.Button();
@@ -39,68 +40,89 @@
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.listBox1 = new System.Windows.Forms.ListBox();
 			this.FileLoadButton = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.RepeatButton = new System.Windows.Forms.Button();
+			this.ShuffleButton = new System.Windows.Forms.Button();
+			this.ComListBox = new System.Windows.Forms.ListBox();
+			this.ComResultTextBox = new System.Windows.Forms.TextBox();
+			this.Connect = new System.Windows.Forms.Button();
+			this.Disconnect = new System.Windows.Forms.Button();
+			this.MyPort = new System.IO.Ports.SerialPort(this.components);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// PlayButton
 			// 
 			this.PlayButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.PlayButton.FlatAppearance.BorderSize = 0;
+			this.PlayButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.PlayButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
 			this.PlayButton.Image = ((System.Drawing.Image)(resources.GetObject("PlayButton.Image")));
-			this.PlayButton.Location = new System.Drawing.Point(384, 381);
+			this.PlayButton.Location = new System.Drawing.Point(382, 382);
 			this.PlayButton.Name = "PlayButton";
 			this.PlayButton.Size = new System.Drawing.Size(54, 57);
-			this.PlayButton.TabIndex = 0;
+			this.PlayButton.TabIndex = 4;
 			this.PlayButton.UseVisualStyleBackColor = false;
 			this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
 			// 
 			// ffButton
 			// 
+			this.ffButton.FlatAppearance.BorderSize = 0;
+			this.ffButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.ffButton.Image = ((System.Drawing.Image)(resources.GetObject("ffButton.Image")));
-			this.ffButton.Location = new System.Drawing.Point(444, 381);
+			this.ffButton.Location = new System.Drawing.Point(481, 382);
 			this.ffButton.Name = "ffButton";
 			this.ffButton.Size = new System.Drawing.Size(68, 57);
-			this.ffButton.TabIndex = 1;
+			this.ffButton.TabIndex = 5;
 			this.ffButton.UseVisualStyleBackColor = true;
 			this.ffButton.Click += new System.EventHandler(this.ffButton_Click);
 			// 
 			// rwButton
 			// 
+			this.rwButton.FlatAppearance.BorderSize = 0;
+			this.rwButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.rwButton.Image = ((System.Drawing.Image)(resources.GetObject("rwButton.Image")));
-			this.rwButton.Location = new System.Drawing.Point(305, 381);
+			this.rwButton.Location = new System.Drawing.Point(238, 379);
 			this.rwButton.Name = "rwButton";
 			this.rwButton.Size = new System.Drawing.Size(73, 57);
-			this.rwButton.TabIndex = 2;
+			this.rwButton.TabIndex = 3;
 			this.rwButton.UseVisualStyleBackColor = true;
 			this.rwButton.Click += new System.EventHandler(this.rwButton_Click);
 			// 
 			// skipForwardButton
 			// 
+			this.skipForwardButton.FlatAppearance.BorderSize = 0;
+			this.skipForwardButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.skipForwardButton.Image = ((System.Drawing.Image)(resources.GetObject("skipForwardButton.Image")));
-			this.skipForwardButton.Location = new System.Drawing.Point(518, 381);
+			this.skipForwardButton.Location = new System.Drawing.Point(574, 382);
 			this.skipForwardButton.Name = "skipForwardButton";
 			this.skipForwardButton.Size = new System.Drawing.Size(67, 57);
-			this.skipForwardButton.TabIndex = 3;
+			this.skipForwardButton.TabIndex = 6;
 			this.skipForwardButton.UseVisualStyleBackColor = true;
 			this.skipForwardButton.Click += new System.EventHandler(this.skipForwardButton_Click);
 			// 
 			// skipBackButton
 			// 
+			this.skipBackButton.FlatAppearance.BorderSize = 0;
+			this.skipBackButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.skipBackButton.Image = ((System.Drawing.Image)(resources.GetObject("skipBackButton.Image")));
-			this.skipBackButton.Location = new System.Drawing.Point(236, 381);
+			this.skipBackButton.Location = new System.Drawing.Point(169, 379);
 			this.skipBackButton.Name = "skipBackButton";
 			this.skipBackButton.Size = new System.Drawing.Size(63, 57);
-			this.skipBackButton.TabIndex = 4;
+			this.skipBackButton.TabIndex = 2;
 			this.skipBackButton.UseVisualStyleBackColor = true;
 			this.skipBackButton.Click += new System.EventHandler(this.skipBackButton_Click);
 			// 
 			// PauseButton
 			// 
+			this.PauseButton.FlatAppearance.BorderSize = 0;
+			this.PauseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.PauseButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
 			this.PauseButton.Image = ((System.Drawing.Image)(resources.GetObject("PauseButton.Image")));
-			this.PauseButton.Location = new System.Drawing.Point(384, 382);
+			this.PauseButton.Location = new System.Drawing.Point(382, 380);
 			this.PauseButton.Name = "PauseButton";
 			this.PauseButton.Size = new System.Drawing.Size(54, 55);
-			this.PauseButton.TabIndex = 5;
+			this.PauseButton.TabIndex = 4;
 			this.PauseButton.UseVisualStyleBackColor = true;
 			this.PauseButton.Visible = false;
 			this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
@@ -129,7 +151,7 @@
 			this.listBox1.Location = new System.Drawing.Point(668, 12);
 			this.listBox1.Name = "listBox1";
 			this.listBox1.Size = new System.Drawing.Size(120, 228);
-			this.listBox1.TabIndex = 9;
+			this.listBox1.TabIndex = 8;
 			// 
 			// FileLoadButton
 			// 
@@ -137,17 +159,96 @@
 			this.FileLoadButton.Location = new System.Drawing.Point(668, 265);
 			this.FileLoadButton.Name = "FileLoadButton";
 			this.FileLoadButton.Size = new System.Drawing.Size(120, 23);
-			this.FileLoadButton.TabIndex = 10;
+			this.FileLoadButton.TabIndex = 9;
 			this.FileLoadButton.Text = "Load File";
 			this.FileLoadButton.UseVisualStyleBackColor = true;
 			this.FileLoadButton.Click += new System.EventHandler(this.FileLoadButton_Click);
 			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(305, 353);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(207, 23);
+			this.progressBar1.TabIndex = 11;
+			// 
+			// RepeatButton
+			// 
+			this.RepeatButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.RepeatButton.FlatAppearance.BorderSize = 0;
+			this.RepeatButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.RepeatButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.RepeatButton.Image = ((System.Drawing.Image)(resources.GetObject("RepeatButton.Image")));
+			this.RepeatButton.Location = new System.Drawing.Point(668, 382);
+			this.RepeatButton.Margin = new System.Windows.Forms.Padding(0);
+			this.RepeatButton.Name = "RepeatButton";
+			this.RepeatButton.Size = new System.Drawing.Size(64, 51);
+			this.RepeatButton.TabIndex = 7;
+			this.RepeatButton.UseVisualStyleBackColor = true;
+			this.RepeatButton.Click += new System.EventHandler(this.RepeatButton_Click);
+			// 
+			// ShuffleButton
+			// 
+			this.ShuffleButton.FlatAppearance.BorderSize = 0;
+			this.ShuffleButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.ShuffleButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.ShuffleButton.Image = ((System.Drawing.Image)(resources.GetObject("ShuffleButton.Image")));
+			this.ShuffleButton.Location = new System.Drawing.Point(79, 388);
+			this.ShuffleButton.Name = "ShuffleButton";
+			this.ShuffleButton.Size = new System.Drawing.Size(54, 45);
+			this.ShuffleButton.TabIndex = 1;
+			this.ShuffleButton.UseVisualStyleBackColor = true;
+			this.ShuffleButton.Click += new System.EventHandler(this.ShuffleButton_Click);
+			// 
+			// ComListBox
+			// 
+			this.ComListBox.FormattingEnabled = true;
+			this.ComListBox.ItemHeight = 16;
+			this.ComListBox.Location = new System.Drawing.Point(13, 31);
+			this.ComListBox.Name = "ComListBox";
+			this.ComListBox.Size = new System.Drawing.Size(120, 84);
+			this.ComListBox.TabIndex = 12;
+			// 
+			// ComResultTextBox
+			// 
+			this.ComResultTextBox.Location = new System.Drawing.Point(13, 134);
+			this.ComResultTextBox.Name = "ComResultTextBox";
+			this.ComResultTextBox.Size = new System.Drawing.Size(120, 22);
+			this.ComResultTextBox.TabIndex = 13;
+			// 
+			// Connect
+			// 
+			this.Connect.ForeColor = System.Drawing.Color.Black;
+			this.Connect.Location = new System.Drawing.Point(13, 163);
+			this.Connect.Name = "Connect";
+			this.Connect.Size = new System.Drawing.Size(120, 23);
+			this.Connect.TabIndex = 14;
+			this.Connect.Text = "Connect";
+			this.Connect.UseVisualStyleBackColor = true;
+			this.Connect.Click += new System.EventHandler(this.Connect_Click);
+			// 
+			// Disconnect
+			// 
+			this.Disconnect.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.Disconnect.Location = new System.Drawing.Point(13, 193);
+			this.Disconnect.Name = "Disconnect";
+			this.Disconnect.Size = new System.Drawing.Size(120, 23);
+			this.Disconnect.TabIndex = 15;
+			this.Disconnect.Text = "Disconnect";
+			this.Disconnect.UseVisualStyleBackColor = true;
+			this.Disconnect.Click += new System.EventHandler(this.Disconnect_Click);
+			// 
 			// Form1
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.Disconnect);
+			this.Controls.Add(this.Connect);
+			this.Controls.Add(this.ComResultTextBox);
+			this.Controls.Add(this.ComListBox);
+			this.Controls.Add(this.ShuffleButton);
+			this.Controls.Add(this.RepeatButton);
+			this.Controls.Add(this.progressBar1);
 			this.Controls.Add(this.FileLoadButton);
 			this.Controls.Add(this.listBox1);
 			this.Controls.Add(this.pictureBox1);
@@ -180,6 +281,14 @@
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.ListBox listBox1;
 		private System.Windows.Forms.Button FileLoadButton;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Button RepeatButton;
+		private System.Windows.Forms.Button ShuffleButton;
+		private System.Windows.Forms.ListBox ComListBox;
+		private System.Windows.Forms.TextBox ComResultTextBox;
+		private System.Windows.Forms.Button Connect;
+		private System.Windows.Forms.Button Disconnect;
+		private System.IO.Ports.SerialPort MyPort;
 	}
 }
 
--- SERTS GUI/SERTS GUI/Form1.cs
+++ SERTS GUI/SERTS GUI/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.IO.Ports;
 using System.Windows;
 using System.Windows.Forms;
 using MessageBox = System.Windows.Forms.MessageBox;
@@ -16,7 +17,30 @@ namespace SERTS_GUI
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			PopulateListBox(listBox1, @"C:\Users\Zach Jones\source\repos\SERTSAudioPlayer\SERTS GUI\ListBoxFiles", "*.txt");
+			
+			PopulateListBox(listBox1, @"C:\Users\Zach Jones\source\repos\SERTSAudioPlayer\SERTS GUI\ListBoxFiles", "*.txt"); // This allows the user to determine 
+																															 // Get the list of available com ports and put them in the list box
+			string[] ports = SerialPort.GetPortNames();
+			ComResultTextBox.Text = "Com port closed";
+			foreach (string port in ports)
+			{
+				ComListBox.Items.Add(port);                                                                                                                // what file they want to upload.
+			}                                                                                                            // what file they want to upload.
+
+			progressBar1.Minimum = 0;
+			
+			progressBar1.Maximum = 5;
+
+			progressBar1.Step = 1;
+			// Uses a for loop to iterate through the operations to be  
+			// completed. In this case, five files are to be copied into memory,  
+			// so the loop will execute 5 times.  
+			for (int i = 0; i <= 4; i++)
+			{
+				// Inserts code to copy a file  
+				progressBar1.PerformStep();
+				// Updates the label to show that a file was read.  
+			}
 		}  
 
 		private void PlayButton_Click(object sender, EventArgs e)
@@ -59,7 +83,6 @@ namespace SERTS_GUI
 		{
 			
 			string fileName = listBox1.SelectedItem.ToString();
-			//Process.Start(@"C:\\Users\\Zach Jones\\source\\repos\\SERTSAudioPlayer\\SERTS GUI\\ListBoxFile\\" + fileName);
 			MessageBox.Show("You clicked: " + fileName);
 		}
 
@@ -72,8 +95,66 @@ namespace SERTS_GUI
 				lsb.Items.Add(file.Name);
 			}
 		}
+
+		private void RepeatButton_Click(object sender, EventArgs e)
+		{
+			string message = "You clicked Repeat Button";
+			MessageBox.Show(message);
+		}
+
+		private void ShuffleButton_Click(object sender, EventArgs e)
+		{
+			string message = "You clicked Shuffle Button";
+			MessageBox.Show(message);
+		}
+
+		private void Connect_Click(object sender, EventArgs e)
+		{
+			// Connect to a selected serial port
+			if (MyPort.IsOpen == false)
+			{
+				// a serial port in not already open
+				if (ComListBox.SelectedItem != null/* TODO Change to default(_) if this is not a reference type */ )
+				{
+					// an item in the listbox has been selected
+					MyPort.PortName = ComListBox.SelectedItem.ToString();
+
+					try
+					{
+						// try to open the selected serial port
+						MyPort.Open();
+					}
+					catch
+					{
+						Debug.WriteLine("Failed to open serial port");
+					}
+					if (MyPort.IsOpen)
+						// The serial port was opened
+						ComResultTextBox.Text = "Com port " + MyPort.PortName + " connected";
+					else
+						// The serial port open failed
+						ComResultTextBox.Text = "Com port " + MyPort.PortName + " not connected";
+				}
+				else
+					ComResultTextBox.Text = "Nothing selected";
+			}
+			else
+				// A com port is already open
+				ComResultTextBox.Text = "Com port " + MyPort.PortName + " already connected";
+		}
+
+		private void Disconnect_Click(object sender, EventArgs e)
+		{
+			// Close the serial port if it is open
+			if (MyPort.IsOpen)
+			{
+				MyPort.Close();
+				ComResultTextBox.Text = "Com port closed";
+			}
+		}
 	}
+
 }
 
-//insert listBox for file
-// button to load file list
+
+
